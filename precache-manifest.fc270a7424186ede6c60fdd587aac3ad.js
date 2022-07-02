a6cd9fab64166361612598fdf412e68825ea70a0
webst2r <rodrigorodrigues56@hotmail.com>
2022-07-02 17:49:01 +0100

    Updates

---
+++ precache-manifest.fc270a7424186ede6c60fdd587aac3ad.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d969545dd5be6a3d7bf4",
+    "url": "/portfolio/static/css/main.95c4d6ec.chunk.css"
+  },
+  {
+    "revision": "d969545dd5be6a3d7bf4",
+    "url": "/portfolio/static/js/main.d969545d.chunk.js"
+  },
+  {
+    "revision": "43ba1a7991038609b197",
+    "url": "/portfolio/static/js/1.43ba1a79.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "13301115b69aa0678bd613f9da7e3174",
+    "url": "/portfolio/static/media/cv.13301115.pdf"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/portfolio/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "89b78b39c6a7dbd1692710c16187cd2e",
+    "url": "/portfolio/static/media/logo.89b78b39.png"
+  },
+  {
+    "revision": "930633b64224ef9fd2a8ab17820959c6",
+    "url": "/portfolio/static/media/portfolio1.930633b6.png"
+  },
+  {
+    "revision": "7282d42fdede2a03ab84f033eda33651",
+    "url": "/portfolio/static/media/portfolio2.7282d42f.png"
+  },
+  {
+    "revision": "1095b2fa8e2cd9a58d294fb6c4adad76",
+    "url": "/portfolio/static/media/portfolio3.1095b2fa.png"
+  },
+  {
+    "revision": "721fd3953bb31181adbd776586c59c69",
+    "url": "/portfolio/static/media/portfolio4.721fd395.png"
+  },
+  {
+    "revision": "5f4f105e0a9cc710257c8ceebba6da6c",
+    "url": "/portfolio/static/media/portfolio5.5f4f105e.jpg"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/portfolio/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "2742979001ff8df0f3d647d688eb6cc4",
+    "url": "/portfolio/index.html"
+  }
+];
